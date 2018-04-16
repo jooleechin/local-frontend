8f5f6ee783d80f99513df2419678f97ad40a1a89
Joolee Chin <jooleechin@gmail.com>
2018-04-16 16:15:29 -0700

    made drag and drop

--- src/components/Itin.js
+++ src/components/Itin.js
@@ -9,6 +9,33 @@ import TrashButton from './TrashButton'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 const moment = require('moment')
 
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+// using some little inline style helpers to make the app look okay
+const grid = 8;
+const getItemStyle = (draggableStyle, isDragging) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  // marginBottom: grid,
+
+  // change background colour if dragging
+  background: isDragging ? 'none' : 'none',
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightgrey' : 'none',
+  width: 340,
+});
+
+
 class Itin extends Component {
   constructor(props) {
     super(props)
@@ -21,12 +48,29 @@ class Itin extends Component {
       itin_id: 0,
     }
     this.toggleEditHidden = this.toggleEditHidden.bind(this)
+    this.onDragEnd = this.onDragEnd.bind(this);
   }
   toggleEditHidden () {
     this.setState(prevState => ({
       editIsHidden: !prevState.editIsHidden
     }))
   }
+
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+    const items = reorder(
+      this.state.allActivity,
+      result.source.index,
+      result.destination.index
+    );
+    this.setState({
+      allActivity: items
+    });
+  }
+
   goBack = () => {
     this.props.history.push('/main')
   }
@@ -38,11 +82,10 @@ class Itin extends Component {
       state: { googlePlace_ID }
     })
   }
-  //deletedPlace.data.place.id
+
   deletePlace = (e) => {
     let place_ID = e.target.dataset.placeID
     localAPI.deletePlace(place_ID).then(deletedPlace => {
-      debugger
       let allActivity = this.state.allActivity.filter(place => place.places_id !== deletedPlace.data.place.id)
       this.setState({allActivity})
     })
@@ -53,32 +96,57 @@ class Itin extends Component {
 
   newActivity = () => {
     return this.state.allActivity.map(activity => (
-      <div className="activity" data-answer={activity.googlePlace_ID}>
-        {console.log(activity)}
-        <div className="activityImg" data-answer={activity.googlePlace_ID} onClick={this.showMore}>
-          <Image src={activity.photoUrl}
-            alt={activity.photoUrl}
-            full={true}
-            fit='contain'
-            size='thumb'
-            data-answer={activity.googlePlace_ID}
-            />
-        </div>
-        <div className="activityDesc tl" onClick={this.showMore}>
-          <div className="name" data-answer={activity.googlePlace_ID}>{activity.places_name}</div>
-          <div className="distance" data-answer={activity.googlePlace_ID}>distance: 5 mi</div>
-        </div>
-        <button><FormClose onClick={this.deletePlace} data-placeID={activity.places_id}/></button>
-      </div>
+      <Draggable key={activity.order} draggableId={activity.order}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            style={getItemStyle(
+              provided.draggableProps.style,
+              snapshot.isDragging
+            )}
+            {...provided.dragHandleProps}
+          >
+            <div className="activity" data-answer={activity.googlePlace_ID}>
+              {console.log(activity)}
+              <div className="activityImg" data-answer={activity.googlePlace_ID} onClick={this.showMore}>
+                <Image src={activity.photoUrl}
+                  alt={activity.photoUrl}
+                  full={true}
+                  fit='contain'
+                  size='thumb'
+                  data-answer={activity.googlePlace_ID}
+                  />
+              </div>
+              <div className="activityDesc tl" onClick={this.showMore}>
+                <div className="name" data-answer={activity.googlePlace_ID}>{activity.places_name}</div>
+                <div className="distance" data-answer={activity.googlePlace_ID}>distance: 5 mi</div>
+              </div>
+              <button><FormClose onClick={this.deletePlace} data-placeID={activity.places_id}/></button>
+            </div>
+          </div>
+        )}
+      </Draggable>
     ))
   }
 
   newTimeOfDay = () => {
     let timeArr = this.state.time.split(',')
+    // timeArr.forEach(time => (
+    //   if (time == 'morning')
+    // ))
     return timeArr.map(time => (
-      <div className="itinContent timeOfDay tl">{time}
-        {this.newActivity()}
-      </div>
+        <div className="itinContent timeOfDay tl">{time}
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+              >
+                {this.newActivity()}
+              </div>
+            )}
+          </Droppable>
+        </div>
     ))
   }
 
@@ -111,17 +179,19 @@ class Itin extends Component {
   render() {
     console.log('itin state', this.state)
     return(
-      <div className="itinBox">
-        <div className="buttTitle">
-          <div className="itinTitle tl">
-            <span>itinerary for <span className="titleKey">{this.state.date}</span> in <span className="titleKey">{this.state.destination}</span></span>
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div className="itinBox">
+          <div className="buttTitle">
+            <div className="itinTitle tl">
+              <span>itinerary for <span className="titleKey">{this.state.date}</span> in <span className="titleKey">{this.state.destination}</span></span>
+            </div>
           </div>
-        </div>
           <div className="itineraryBox">
             {this.newTimeOfDay()}
           </div>
-        <TrashButton itin_id={this.state.itin_id} history={this.props.history}/>
-      </div>
+          {/*<TrashButton itin_id={this.state.itin_id} history={this.props.history}/>*/}
+        </div>
+      </ DragDropContext>
     )
   }
 }
